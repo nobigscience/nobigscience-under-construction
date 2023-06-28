31b7190fa0900ebb29ba532918c0f39bf292fa87
aidasbui <aidasbuivydas@gmail.com>
2023-06-29 01:25:41 +0300

    feat: add loading spinner

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingPage = () => {
+  return (
+    <div className="flex place-items-center h-screen">
+      <div className="border-4 border-solid border-l-color-secondary rounded-[50%] w-10 h-10 animate-spin"/>
+    </div>
+  );
+};
+
+export default LoadingPage;
