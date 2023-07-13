4254533e4a5b668f153f4c4bd95a87f5069ba06c
aidasbui <aidasbuivydas@gmail.com>
2023-07-13 18:19:39 +0300

    fix: make logo responsive

--- src/app/components/LevitatingLogoHeading/LevitatingLogoHeading.tsx
+++ src/app/components/LevitatingLogoHeading/LevitatingLogoHeading.tsx
@@ -5,7 +5,7 @@ import { Levitate } from '@/app/hooks/useLevitate';
 const LevitatingLogoHeading = () => {
   return (
     <Levitate>
-      <h1 className="pb-7">nobigscience</h1>
+      <h1 className="pb-7 text-6xl sm:text-7xl md:text-8xl">nobigscience</h1>
     </Levitate>
   );
 };
